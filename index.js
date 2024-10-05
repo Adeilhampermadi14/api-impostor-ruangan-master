087053be51fb807aad9b78e634cec1c8f1c3b6b0
Ade ilham permadi <permadiadeilham@gmail.com>
2024-10-05 23:48:01 +0700

    Update index.js

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.use(bodyParser.json());
  
 app.use(
   cors({
-    origin: ["https://ruangan-impostor.vercel.app","https://vercel.com/ilhampermadis-projects/proyek-3/GMNrSSrKwRLSkgJEpSiWnV1CJHxm"],
+    origin: ["https://proyek-3-dyuv89q3d-ilhampermadis-projects.vercel.app"],
     methods: ["POST", "PUT", "GET", "DELETE", "OPTIONS", "HEAD"],
     credentials: true,
   })
